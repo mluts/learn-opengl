d712436ecb9249b39ab8648707962cda549d82f9
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2016-12-01 22:47:34 +0200

    in c

--- 1-creating-a-window/main.c
+++ 1-creating-a-window/main.c
@@ -1,6 +1,46 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 
 int main()
 {
+
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+
+  if(window == NULL)
+  {
+    printf("Failed to create GLFW window\n");
+    glfwTerminate();
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  glewExperimental = GL_TRUE;
+
+  if(glewInit() != GLEW_OK)
+  {
+    printf("Failed to initialize GLEW\n");
+    return 1;
+  }
+
+  int width, height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+
+  while(!glfwWindowShouldClose(window))
+  {
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
+
   return 0;
 }
