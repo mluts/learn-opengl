56743b7743392918613994a748e2d366552b3dd4
Michael Lutsiuk <michael.lutsiuk@gmail.com>
2016-12-04 16:23:46 +0200

    square!

--- 3-square/main.cpp
+++ 3-square/main.cpp
@@ -19,9 +19,15 @@ static const char *fragmentShaderSource =
 ;
 
 GLfloat vertices[] = {
-  -0.5f, -0.5f, 0.0f,
-   0.5f, -0.5f, 0.0f,
-   0.0f,  0.5f, 0.0f
+   0.5f,  0.5f, 0.0f, // Top right
+   0.5f, -0.5f, 0.0f, // Bottom right
+  -0.5f, -0.5f, 0.0f, // Bottom left
+  -0.5f,  0.5f, 0.0f  // Top left
+};
+
+GLuint indices[] = {
+  0, 1, 2,
+  3, 0, 2
 };
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
@@ -100,15 +106,19 @@ GLchar infoLog[512];
  */
 void createVertexArray(GLuint *VAO)
 {
-GLuint VBO;
+GLuint VBO, EBO;
 
   glGenVertexArrays(1, VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   glBindVertexArray(*VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
   glEnableVertexAttribArray(0);
@@ -184,7 +194,7 @@ int main()
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     glfwSwapBuffers(window);
